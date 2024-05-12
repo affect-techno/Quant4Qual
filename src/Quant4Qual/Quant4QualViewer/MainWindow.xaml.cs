b2bcf7f41f824db33bdcb7b117d7597202598fd3
affect-techno <zafi.moore@gmail.com>
2024-05-12 15:06:32 +0100

    Updates suggested by LICENSE

--- src/Quant4Qual/Quant4QualViewer/MainWindow.xaml.cs
+++ src/Quant4Qual/Quant4QualViewer/MainWindow.xaml.cs
@@ -1,23 +1,35 @@
-﻿using System.Text;
+﻿// Quant4Qual
+//     Copyright (C) 2024  Zafi Moore
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#region
+
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Quant4QualViewer;
+#endregion
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+namespace Quant4QualViewer
 {
-    public MainWindow()
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
     }
 }
